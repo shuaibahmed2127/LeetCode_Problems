a2368a0a87323b1dba4bcf8bfc80801282438778
shuaibahmed2127 <shuaibahmed2127@gmail.com>
2026-08-15 17:36:05 +0530

    Time: 770 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,4 +1,8 @@
-SELECT p.firstName, p.lastName, a.city, a.state
-FROM Person p
-LEFT JOIN Address a
-ON p.personId = a.personId;
+SELECT
+    Person.firstName,
+    Person.lastName,
+    Address.city,
+    Address.state
+FROM Person
+LEFT JOIN Address
+ON Person.personId = Address.personId;
