78f8d8e21760f42820b4f4bde56ca559d9c217ba
shuaibahmed2127 <shuaibahmed2127@gmail.com>
2026-09-07 18:21:03 +0530

    Time: 8 ms (96.07%), Space: 43 MB (88.46%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -4,9 +4,9 @@ class Solution {
         int[] dp = new int[n+1];
         dp[0] = 1;
         for(char c : s.toCharArray()){
-            for(int j=n; j>=1; j--){
-                if(c == t.charAt(j-1)){
-                    dp[j] = dp[j] + dp[j-1];
+            for(int j = n; j>=1; j--){
+                if(c==t.charAt(j-1)){
+                    dp[j]+=dp[j-1];
                 }
             }
         }
