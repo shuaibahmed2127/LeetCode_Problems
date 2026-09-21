ce70a1280bd46c9e01585ccfa027cf601d7c1dd5
shuaibahmed2127 <shuaibahmed2127@gmail.com>
2026-09-21 21:40:43 +0530

    Time: 0 ms (100%), Space: 47.2 MB (9.15%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,6 +1,6 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        int possibleZeroDuplicates = 0;
+        /*int possibleZeroDuplicates = 0;
         int lastIndex = arr.length - 1;
         for(int i=0; i<=lastIndex-possibleZeroDuplicates; i++){
             if(arr[i] == 0){
@@ -20,6 +20,24 @@ class Solution {
                 arr[i + possibleZeroDuplicates] = 0;
             }else{
                 arr[i + possibleZeroDuplicates] = arr[i];
+            }
+        } */
+        int[] copy = Arrays.copyOf(arr,arr.length);
+        int slow = 0;
+        int fast = 0;
+        while(fast < arr.length){
+            if(copy[slow] == 0){
+                arr[fast] = 0;
+                fast++;
+                if(fast < arr.length){
+                    arr[fast] = 0;
+                fast++;
+                }
+                slow++;
+            }else{
+                arr[fast] = copy[slow];
+                slow++;
+                fast++;
             }
         }
 
